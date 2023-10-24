8d2ec3b480ab4e3d6536eecc8b02e70855fe11f9
pierrelasse <pierrelassebusiness@gmail.com>
2023-10-24 22:33:37 +0200

    feat(StartCmd): the timer resumes now

--- src/main/java/net/bluept/forceitembattle/services/command/commands/StartCmd.java
+++ src/main/java/net/bluept/forceitembattle/services/command/commands/StartCmd.java
@@ -1,8 +1,9 @@
 package net.bluept.forceitembattle.services.command.commands;
 
+import net.bluept.forceitembattle.ForceItemBattle;
 import net.bluept.forceitembattle.services.command.Command;
+import net.bluept.forceitembattle.services.timer.TimerService;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 import java.util.List;
 
@@ -13,8 +14,9 @@ public class StartCmd extends Command {
 
     @Override
     public void execute(CommandSender sender, List<String> args) {
-        if (sender instanceof Player) {
-            sender.sendMessage("Hola!");
+        TimerService timerService = ForceItemBattle.INSTANCE.serviceManager.getServiceHandle("timer", TimerService.class);
+        if (timerService != null) {
+            timerService.resumed = true;
         }
     }
 }
