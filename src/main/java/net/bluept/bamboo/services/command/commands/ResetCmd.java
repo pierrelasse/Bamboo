78b5fe4bbe48bfd2da6c12fe3b30049b518019f2
pierrelasse <pierrelassebusiness@gmail.com>
2023-10-30 19:56:26 +0100

    feat(ResetCmd): require /reset confirm

--- src/main/java/net/bluept/bamboo/services/command/commands/ResetCmd.java
+++ src/main/java/net/bluept/bamboo/services/command/commands/ResetCmd.java
@@ -21,28 +21,37 @@ public class ResetCmd extends Command {
     @SuppressWarnings("deprecation")
     @Override
     public void execute(CommandSender sender, List<String> args) {
+        if ("confirm".equals(Utils.get(args, 0))) {
+            kickAllPlayers(sender);
+
+            Set<String> blacklistedFolders = Set.of("datapacks", "paper-world.yml");
+            for (String world : List.of("world", "world_nether", "world_the_end")) {
+                File folder = new File(Bamboo.INS.serverRoot, world);
+                File[] files = folder.listFiles();
+                if (files != null) {
+                    for (File file : files) {
+                        if (!blacklistedFolders.contains(file.getName())) {
+                            Utils.rDelete(file);
+                        }
+                    }
+                }
+            }
+
+            Bamboo.INS.serviceManager.getAndRun(TimerService.class, serv -> {
+                serv.time = 0;
+            });
+
+            Bukkit.shutdown();
+
+        } else {
+            Utils.send(sender, "&cBitte benutze /reset confirm");
+        }
+    }
+
+    public void kickAllPlayers(CommandSender sender) {
         String message = Utils.colorfy("&<#E83845>Der Server wird nun zur\u00FCckgesetzt.\n&<#D62B38>Von " + sender.getName());
         for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
             onlinePlayer.kickPlayer(message);
         }
-
-        Set<String> blacklistedFolders = Set.of("datapacks", "paper-world.yml");
-        for (String world : List.of("world", "world_nether", "world_the_end")) {
-            File folder = new File(Bamboo.INS.serverRoot, world);
-            File[] files = folder.listFiles();
-            if (files != null) {
-                for (File file : files) {
-                    if (!blacklistedFolders.contains(file.getName())) {
-                        Utils.rDelete(file);
-                    }
-                }
-            }
-        }
-
-        Bamboo.INS.serviceManager.getAndRun(TimerService.class, serv -> {
-            serv.time = 0;
-        });
-
-        Bukkit.shutdown();
     }
 }
