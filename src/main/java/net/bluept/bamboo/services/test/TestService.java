d346baaa03acac68a2134cc01e98860be164a6ac
pierrelasse <pierrelassebusiness@gmail.com>
2023-11-04 21:08:24 +0100

    feat(TestService): finish for now

--- src/main/java/net/bluept/bamboo/services/test/TestService.java
+++ src/main/java/net/bluept/bamboo/services/test/TestService.java
@@ -5,6 +5,10 @@ import net.bluept.bamboo.service.Service;
 import net.bluept.bamboo.service.ServiceInfo;
 import net.bluept.bamboo.util.Utils;
 import org.bukkit.Bukkit;
+import org.bukkit.SoundCategory;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarStyle;
+import org.bukkit.boss.BossBar;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitTask;
 
@@ -12,6 +16,7 @@ import org.bukkit.scheduler.BukkitTask;
 public class TestService extends Service {
     private BukkitTask tickTask;
     private int stage = 0;
+    private BossBar timerBossbar;
 
     @Override
     public void onEnable() {
@@ -23,6 +28,7 @@ public class TestService extends Service {
             tickTask.cancel();
             tickTask = null;
         }
+        clearBossbar();
     }
 
     @Override
@@ -32,6 +38,7 @@ public class TestService extends Service {
 
     public void startTest() {
         if (tickTask == null) {
+            onDisable();
             stage = 0;
             tickTask = Bukkit.getScheduler().runTaskTimer(Bamboo.INS, this::tick, 0, 1);
         }
@@ -39,22 +46,52 @@ public class TestService extends Service {
 
     private void tick() {
         switch (++stage) {
-            case 1:
+            case 1 -> {
                 sendToAll("&7&oSystem test started");
-                break;
-
-            case 2:
+            }
+            case 5 -> {
+                sendToAll("&8&oStarting bossbar test");
+                timerBossbar = Bukkit.createBossBar(Utils.colorfy("&4&k## &c&lDU SOLLTEST NUR DIESEN TEXT SEHEN! &4&k##"), BarColor.GREEN, BarStyle.SOLID);
+                timerBossbar.setProgress(0);
+                Bukkit.getOnlinePlayers().forEach(timerBossbar::addPlayer);
+            }
+            case 60 -> {
+                clearBossbar();
+                sendToAll("&2Bossbar test done");
+            }
+            case 70 -> {
+                sendToAll("&8&oStarting char test");
+                for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                    Utils.title(onlinePlayer, "\u7004", Utils.colorfy("&cEtwas ist schiefgelaufen!"), 5, 20, 5);
+                }
+            }
+            case 100 -> {
+                sendToAll("&2Char test done\n&8&oStarting sound test");
+                for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                    onlinePlayer.playSound(onlinePlayer.getLocation(), "bluept:test", SoundCategory.VOICE, 1F, 1F);
+                }
+            }
+            case 120 -> {
+                sendToAll("&2Sound test done");
+            }
+            case 140 -> {
                 onDisable();
                 sendToAll("&aTest done");
-                break;
+            }
         }
     }
 
     private void sendToAll(String s) {
         String message = Utils.colorfy(s);
-
         for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
             onlinePlayer.sendMessage(message);
+        }
+    }
+
+    private void clearBossbar() {
+        if (timerBossbar != null) {
+            timerBossbar.removeAll();
+            timerBossbar = null;
         }
     }
 }
