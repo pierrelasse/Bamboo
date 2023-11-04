e8b413fc7176c47771b2e59fd757ac42d705d52e
pierrelasse <pierrelassebusiness@gmail.com>
2023-11-04 12:30:23 +0100

    fix(Emojis): rename PeepoHey->peepoHey

--- src/main/java/net/bluept/bamboo/services/emoji/Emojis.java
+++ src/main/java/net/bluept/bamboo/services/emoji/Emojis.java
@@ -2,5 +2,5 @@ package net.bluept.bamboo.services.emoji;
 
 public class Emojis {
     public static final String MsTeams = "\u7006";
-    public static final String PeepoHey = "\u7010";
+    public static final String peepoHey = "\u7010";
 }
