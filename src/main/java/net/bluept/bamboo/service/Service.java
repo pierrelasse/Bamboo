7be30d28099fa126899125f909f7dfc2f6d8df34
pierrelasse <pierrelassebusiness@gmail.com>
2023-11-04 15:18:46 +0100

    feat(Service): load data from annotation

--- src/main/java/net/bluept/bamboo/service/Service.java
+++ src/main/java/net/bluept/bamboo/service/Service.java
@@ -1,7 +1,46 @@
 package net.bluept.bamboo.service;
 
+import java.util.Objects;
+
 public abstract class Service {
+    public final String name;
+    public final String description;
     protected boolean enabled = false;
+
+    public Service() {
+        ServiceInfo info = getClass().getAnnotation(ServiceInfo.class);
+
+        String name = null;
+        String description = null;
+
+        if (info != null) {
+            name = info.name();
+            description = info.description();
+            if (Objects.equals(description, "")) {
+                description = null;
+            }
+        }
+
+        if (name == null || name.equals("")) {
+            name = getId(getClass());
+        }
+
+        this.name = name;
+        this.description = description;
+    }
+
+    public static String getId(Class<? extends Service> clazz) {
+        ServiceInfo info = clazz.getAnnotation(ServiceInfo.class);
+        if (info != null) {
+            String id = info.id();
+            if (!Objects.equals(id, "")) {
+                return id;
+            }
+        }
+        String className = clazz.getSimpleName();
+        int lastIndex = className.lastIndexOf("Service");
+        return (lastIndex != -1 ? className.substring(0, lastIndex) : className).toLowerCase();
+    }
 
     public boolean isEnabled() {
         return enabled;
