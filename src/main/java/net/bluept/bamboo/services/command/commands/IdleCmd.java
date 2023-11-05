a8ccf581b9ba25780abfbc045ca73668c61050f9
pierrelasse <pierrelassebusiness@gmail.com>
2023-11-05 13:28:31 +0100

    feat(IdleCmd): add start/stop sound (https://github.com/pierrelasse/Bamboo/issues/19)

--- src/main/java/net/bluept/bamboo/services/command/commands/IdleCmd.java
+++ src/main/java/net/bluept/bamboo/services/command/commands/IdleCmd.java
@@ -3,7 +3,10 @@ package net.bluept.bamboo.services.command.commands;
 import net.bluept.bamboo.Bamboo;
 import net.bluept.bamboo.services.command.Command;
 import net.bluept.bamboo.services.timer.TimerService;
+import org.bukkit.Bukkit;
+import org.bukkit.SoundCategory;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import java.util.List;
 
@@ -16,6 +19,9 @@ public class IdleCmd extends Command {
     public void execute(CommandSender sender, List<String> args) {
         Bamboo.INS.serviceManager.getAndRun(TimerService.class, serv -> {
             serv.resumed = !serv.resumed;
+            for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                onlinePlayer.playSound(onlinePlayer.getLocation(), "bluept:notification", SoundCategory.VOICE, 1F, 1F);
+            }
         });
     }
 }
