1ed896dff4858db18c3b469797ffcbd25015a53f
pierrelasse <pierrelassebusiness@gmail.com>
2023-11-17 15:53:09 +0100

    feat(DimTPDevCmd): update, add setenabled subcommand

--- src/main/java/net/bluept/bamboo/services/dimtp/commands/DimTPDevCmd.java
+++ src/main/java/net/bluept/bamboo/services/dimtp/commands/DimTPDevCmd.java
@@ -18,7 +18,7 @@ public class DimTPDevCmd extends Command {
     public DimTPDevCmd() {
         super("dimtpdev");
         setPermission("penis");
-        usage("/dimtp (tpmenow|tpallnow|tpallsoon|settick|info) ...");
+        usage("/dimtpdev (tpmenow|tpallnow|tpallsoon|settick|info) ...");
     }
 
     @Override
@@ -31,58 +31,69 @@ public class DimTPDevCmd extends Command {
 
         String subCommand = Utils.get(args, 0);
 
-        if ("tpmenow".equals(subCommand)) {
-            Object[] data = Generator.getRandomLocation(Generator.randomDim(), 0);
-            if (data[0] == null) {
-                Utils.send(player, "&cCould not find a location");
-                return;
+        switch (subCommand) {
+            case "tpmenow" -> {
+                Object[] data = Generator.getRandomLocation(Generator.randomDim(), 0);
+                if (data[0] == null) {
+                    Utils.send(player, "&cCould not find a location");
+                    return;
+                }
+                player.teleport((Location)data[0]);
+                Utils.send(player, "&aTeleported randomly. After " + data[1] + " tries");
             }
-            player.teleport((Location)data[0]);
-            Utils.send(player, "&aTeleported randomly. After " + data[1] + " tries");
-
-        } else if ("tpallnow".equals(subCommand)) {
-            dimTPService.tick = DimTPConfig.INTERVAL;
-            Utils.send(player, "&aSet tick");
-
-        } else if ("tpallsoon".equals(subCommand)) {
-            dimTPService.tick = DimTPConfig.INTERVAL - 3;
-            Utils.send(player, "&aSet tick to tp soon");
-
-        } else if ("settick".equals(subCommand)) {
-            Integer tick = null;
-            try {
-                tick = Integer.parseInt(Utils.get(args, 1));
-            } catch (NumberFormatException ignored) {
+            case "tpallnow" -> {
+                dimTPService.tick = DimTPConfig.INTERVAL;
+                Utils.send(player, "&aSet tick");
             }
-
-            if (tick == null || tick > 500000 || tick < 0) {
-                Utils.send(player, "&cInvalid number");
-                return;
+            case "tpallsoon" -> {
+                dimTPService.tick = DimTPConfig.INTERVAL - 3;
+                Utils.send(player, "&aSet tick to tp soon");
             }
+            case "settick" -> {
+                Integer tick = null;
+                try {
+                    tick = Integer.parseInt(Utils.get(args, 1));
+                } catch (NumberFormatException ignored) {
+                }
 
-            dimTPService.tick = tick;
+                if (tick == null || tick > 500000 || tick < 0) {
+                    Utils.send(player, "&cInvalid number");
+                    return;
+                }
 
-            Utils.send(player, "&aSet tick to " + tick);
+                dimTPService.tick = tick;
 
-        } else if ("info".equals(subCommand)) {
-            Utils.send(player, "&dDimTP info&8:");
-            Utils.send(player, "&d  Tick&8: &c" + dimTPService.tick);
-            Utils.send(player, "&d  Interval&8: &5" + DimTPConfig.INTERVAL);
-            Utils.send(player, "&d  Interval Min&8: &5" + DimTPConfig.INTERVAL_MIN);
-            Utils.send(player, "&d  Interval Max&8: &5" + DimTPConfig.INTERVAL_MAX);
-            Utils.send(player, "&d  Tp in&8: &a" + DisplayHelper.convertSecondsToDuration(DimTPConfig.INTERVAL - dimTPService.tick));
-            Utils.send(player, "&d  Max tries&8: &4" + DimTPConfig.MAX_TRIES);
-            Utils.send(player, "&d  X_MAX&8: &e" + DimTPConfig.X_MAX);
-            Utils.send(player, "&d  X_MIN&8: &e" + DimTPConfig.X_MIN);
-            Utils.send(player, "&d  Z_MAX&8: &e" + DimTPConfig.Z_MAX);
-            Utils.send(player, "&d  Z_MIN&8: &e" + DimTPConfig.Z_MIN);
-
-        } else if ("dis".equals(subCommand)) {
-            DimTPConfig.enabled = false;
-            Utils.send(player, "OK!");
-
-        } else {
-            Utils.send(player, usage());
+                Utils.send(player, "&aSet tick to " + tick);
+            }
+            case "info" -> {
+                Utils.send(player, "&dDimTP info&8:");
+                Utils.send(player, "&d  Tick&8: &c" + dimTPService.tick);
+                Utils.send(player, "&d  Interval&8: &5" + DimTPConfig.INTERVAL);
+                Utils.send(player, "&d  Interval Min&8: &5" + DimTPConfig.INTERVAL_MIN);
+                Utils.send(player, "&d  Interval Max&8: &5" + DimTPConfig.INTERVAL_MAX);
+                Utils.send(player, "&d  Tp in&8: &a" + DisplayHelper.convertSecondsToDuration(DimTPConfig.INTERVAL - dimTPService.tick));
+                Utils.send(player, "&d  Max tries&8: &4" + DimTPConfig.MAX_TRIES);
+                Utils.send(player, "&d  X_MAX&8: &e" + DimTPConfig.X_MAX);
+                Utils.send(player, "&d  X_MIN&8: &e" + DimTPConfig.X_MIN);
+                Utils.send(player, "&d  Z_MAX&8: &e" + DimTPConfig.Z_MAX);
+                Utils.send(player, "&d  Z_MIN&8: &e" + DimTPConfig.Z_MIN);
+            }
+            case "setenabled" -> {
+                String arg1 = Utils.get(args, 1);
+                switch (arg1) {
+                    case "false", "true" -> {
+                        boolean state = arg1.equalsIgnoreCase("true");
+                        DimTPConfig.enabled = state;
+                        Utils.send(player, "&aTeleporting is now " + (state ? "enabled" : "disabled"));
+                    }
+                    default -> {
+                        Utils.send(player, "&cUsage: /dimtpdev setenabled (true|false)");
+                    }
+                }
+            }
+            default -> {
+                Utils.send(player, usage());
+            }
         }
     }
 
@@ -92,7 +103,7 @@ public class DimTPDevCmd extends Command {
         String arg0 = Utils.get(args, 0, "");
 
         if (args.size() < 2) {
-            for (String s : List.of("tpmenow", "tpallnow", "tpallsoon", "settick", "info")) {
+            for (String s : List.of("tpmenow", "tpallnow", "tpallsoon", "settick", "info", "setenabled")) {
                 if (s.startsWith(arg0)) {
                     completions.add(s);
                 }
