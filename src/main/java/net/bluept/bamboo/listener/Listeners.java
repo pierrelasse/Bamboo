5aa2279db57f129ad8a49f03bbb4f4243bf1c77a
pierrelasse <pierrelassebusiness@gmail.com>
2023-11-17 15:54:22 +0100

    feat(Listeners): check if player's game language is supported on join

--- src/main/java/net/bluept/bamboo/listener/Listeners.java
+++ src/main/java/net/bluept/bamboo/listener/Listeners.java
@@ -4,6 +4,8 @@ import net.bluept.bamboo.Bamboo;
 import net.bluept.bamboo.services.emoji.StaticEmoji;
 import net.bluept.bamboo.services.forceitembattle.ItemService;
 import net.bluept.bamboo.services.timer.TimerService;
+import net.bluept.bamboo.services.translation.TranslationService;
+import net.bluept.bamboo.util.Utils;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -11,6 +13,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import java.util.Map;
 
 public class Listeners implements Listener {
     @EventHandler
@@ -39,5 +44,25 @@ public class Listeners implements Listener {
     @EventHandler
     public void event(PlayerChatEvent event) {
         StaticEmoji.handleChatEvent(event);
+    }
+
+    @SuppressWarnings("deprecation")
+    @EventHandler
+    public void event(PlayerJoinEvent event) {
+        TranslationService translationService = Bamboo.INS.serviceManager.getService(TranslationService.class);
+        if (translationService != null) {
+            String lang = Utils.stringifyLocale(event.getPlayer().locale());
+            if (!translationService.loadedTranslations.containsKey(lang)) {
+                StringBuilder sb = new StringBuilder();
+                sb.append("&<#ED0D4C>Invalid game language detected!\n\n&<#F314AC>Supported languages:\n");
+
+                for (Map.Entry<String, Map<String, String>> entry : translationService.loadedTranslations.entrySet()) {
+                    Map<String, String> translations = entry.getValue();
+                    sb.append(String.format("&8• &<#FC07B4>%s (%s) &7- &<#610981>%s", translations.get("language.name"), translations.get("language.region"), entry.getKey()));
+                }
+
+                event.getPlayer().kickPlayer(Utils.colorfy(sb.toString()));
+            }
+        }
     }
 }
