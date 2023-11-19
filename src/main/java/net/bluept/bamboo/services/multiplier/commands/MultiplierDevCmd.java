d3de505a70bc3d1cd53f7caec2f2a9ce2563b5cf
pierrelasse <pierrelassebusiness@gmail.com>
2023-11-19 12:07:42 +0100

    feat(MultiplierDevCmd): add reload and info subcmds

--- src/main/java/net/bluept/bamboo/services/multiplier/commands/MultiplierDevCmd.java
+++ src/main/java/net/bluept/bamboo/services/multiplier/commands/MultiplierDevCmd.java
@@ -6,22 +6,52 @@ import net.bluept.bamboo.services.multiplier.MultiplierService;
 import net.bluept.bamboo.util.Utils;
 import org.bukkit.command.CommandSender;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MultiplierDevCmd extends Command {
     public MultiplierDevCmd() {
         super("MultiplierDev");
+        usage("(reload|info)");
     }
 
     @Override
     public void execute(CommandSender sender, List<String> args) {
-        String arg0 = Utils.get(args, 0);
+        switch (Utils.get(args, 0)) {
+            case "reload" -> {
+                MultiplierService multiplierService = Bamboo.INS.serviceManager.getService(MultiplierService.class);
+                if (multiplierService != null) {
+                    multiplierService.config.load();
+                    Utils.send(sender, "&aReloaded successfully!");
+                }
+            }
+            case "info" -> {
+                MultiplierService multiplierService = Bamboo.INS.serviceManager.getService(MultiplierService.class);
+                if (multiplierService == null) {
+                    Utils.send(sender, "&cUnable to connect to the multiplier service");
+                    break;
+                }
 
-        if ("reload".equals(arg0)) {
-            MultiplierService multiplierService = Bamboo.INS.serviceManager.getService(MultiplierService.class);
-            if (multiplierService != null) {
-                multiplierService.config.load();
+                Utils.send(sender, "&dMultiplier info&8:");
+                Utils.send(sender, "&d  Current multiplier&8: &c" + multiplierService.multiplier);
+            }
+            default -> Utils.send(sender, usage());
+        }
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, String alias, List<String> args) {
+        List<String> completions = new ArrayList<>();
+
+        if (args.size() <= 1) {
+            String arg0 = Utils.get(args, 0, "");
+            for (String s : List.of("reload", "info")) {
+                if (s.startsWith(arg0)) {
+                    completions.add(s);
+                }
             }
         }
+
+        return completions;
     }
 }
