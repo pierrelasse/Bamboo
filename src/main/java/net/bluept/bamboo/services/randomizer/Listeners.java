caf3cdee25728a16ca00c9d29a8239d867ee6992
pierrelasse <pierrelassebusiness@gmail.com>
2023-12-06 20:38:47 +0100

    feat(randomizer service): invrandomizer improvements

--- src/main/java/net/bluept/bamboo/services/randomizer/Listeners.java
+++ src/main/java/net/bluept/bamboo/services/randomizer/Listeners.java
@@ -2,7 +2,6 @@ package net.bluept.bamboo.services.randomizer;
 
 import net.bluept.bamboo.Bamboo;
 import net.bluept.bamboo.services.timer.TimerService;
-import net.bluept.bamboo.util.Utils;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,14 +13,14 @@ public class Listeners implements Listener {
 
     @EventHandler
     public void event(EntityDamageEvent event) {
-        RandomizerService randomizerService = Bamboo.INS.serviceManager.getService(RandomizerService.class);
-        InvRandomizerService invRandomizerService = Bamboo.INS.serviceManager.getService(InvRandomizerService.class);
-        if (randomizerService == null || invRandomizerService == null || !TimerService.isResumed() || !randomizerService.config.get().getBoolean(invRandomizerService.configPrefix + "on_damage")) {
-            return;
-        }
-        if (event.getEntity() instanceof Player player) {
+        if (event.getDamage() > 0 && event.getEntity() instanceof Player player) {
+            RandomizerService randomizerService = Bamboo.INS.serviceManager.getService(RandomizerService.class);
+            InvRandomizerService invRandomizerService = Bamboo.INS.serviceManager.getService(InvRandomizerService.class);
+            if (randomizerService == null || invRandomizerService == null || !TimerService.isResumed()
+                    || !randomizerService.config.get().getBoolean(invRandomizerService.configPrefix + "on_damage")) {
+                return;
+            }
             invRandomizerService.randomizePlayer(player);
-            Utils.send(player, "<%dDamageLP> Huch, da hab ich wohl deine Items durch andere ausgetauscht :/".replace("%d", event.getCause().name().toLowerCase()));
         }
     }
 }
