52ccc2f421e3cac90d515d1a4e094468f870255f
pierrelasse <pierrelassebusiness@gmail.com>
2024-10-22 15:31:18 +0200

    feat(Apperance Service): update pack url

--- src/main/java/net/bluept/bamboo/services/system/appearance/AppearanceService.java
+++ src/main/java/net/bluept/bamboo/services/system/appearance/AppearanceService.java
@@ -5,7 +5,7 @@ import net.bluept.bamboo.service.Service;
 import org.bukkit.event.HandlerList;
 
 public class AppearanceService extends Service {
-    public final String pack_url = "https://download.mc-packs.net/pack/79bceeb09259117a744fb45a7d77a4b0d5ccb493.zip";
+    public final String pack_url = "https://bluept.net/cdn2/Bamboo-Resources.zip";
     public final String pack_sha1 = "79bceeb09259117a744fb45a7d77a4b0d5ccb493";
 
     private Listeners listeners;
